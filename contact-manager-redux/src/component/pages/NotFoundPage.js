cefbc1ecf4c883f9184d183006c6c94aa4e310c9
Tien Duy NGUYEN <39387827+tienduy-nguyen@users.noreply.github.com>
2020-05-30 17:46:53 +0200

    Minor fix

--- contact-manager-redux/src/component/pages/NotFoundPage.js
+++ contact-manager-redux/src/component/pages/NotFoundPage.js
@@ -2,9 +2,9 @@ import React from 'react';
 
 export default function NotFoundPage() {
   return (
-    <div>
-      <h1 className='display-4'>404 Page not found</h1>
-      <p className='lead'>Sorry, that page doest not exist</p>
+    <div className='alert alert-warning text-center'>
+      <h4 className='display-4'>Error 404 - Page not found</h4>
+      <p className='lead'>Sorry, that page does not exist</p>
     </div>
   );
 }
