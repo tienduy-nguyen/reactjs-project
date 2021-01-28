52110038af27fa4df412ad07c2e52cf6f0f6a662
tienduy-nguyen <tienduy.nguyen.dev@gmail.com>
2021-01-28 17:31:03 +0100

    react-testing: Update testing but failed --> to try more

--- react-testing/client/src/App.test.tsx
+++ react-testing/client/src/App.test.tsx
@@ -1,9 +1,132 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import {
+  render,
+  fireEvent,
+  waitFor,
+  waitForElementToBeRemoved,
+} from '@testing-library/react';
 import App from './App';
+import { act } from 'react-dom/test-utils';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+jest.useFakeTimers();
+
+describe('App', () => {
+  beforeEach(() => {
+    fetchMock.resetMocks();
+  });
+  describe('creating', () => {
+    it('should be able to create a book', async () => {
+      fetchMock
+        .mockResponseOnce(JSON.stringify([]))
+        .mockResponseOnce(
+          JSON.stringify({ id: '1', name: 'The Great Gatsby' })
+        );
+
+      const { getByText, getByLabelText } = render(<App />);
+
+      // we "type" in the value
+      const input = getByLabelText('Add a book');
+      fireEvent.change(input, {
+        target: {
+          value: 'The Great Gatsby',
+        },
+      });
+
+      getByText('Submit').click();
+
+      // we submit form, and wait for API response
+      // we should now have in the DOM our Book
+      // and it's delete and edit buttons
+      await waitFor(() => getByText('The Great Gatsby'));
+      expect(getByText('Edit')).toBeDefined();
+      expect(getByText('Delete')).toBeDefined();
+    });
+  });
+
+  describe('editing', () => {
+    it('should be able to edit a book', async () => {
+      fetchMock
+        .mockResponseOnce(
+          JSON.stringify([{ id: '1', name: 'The Great Gatsby' }])
+        )
+        .mockResponseOnce(JSON.stringify({ id: '1', name: 'Harry Potter' }));
+
+      const { getByTestId, getByText } = render(<App />);
+
+      const editButton = await waitFor(() => getByText('Edit'));
+      editButton.click();
+
+      const editInput = getByTestId('edit-input');
+      fireEvent.change(editInput, {
+        target: {
+          value: 'Harry Potter',
+        },
+      });
+
+      getByText('Save').click();
+
+      await waitFor(() => getByText('Harry Potter'));
+      expect(getByText('Edit')).toBeDefined();
+      expect(getByText('Delete')).toBeDefined();
+    });
+  });
+
+  const harryPotter = {
+    id: '1',
+    name: 'Harry Potter',
+  };
+  const greatGatsby = {
+    id: '2',
+    name: 'The Great Gatsby',
+  };
+
+  describe('searching', () => {
+    it.only('should be able to search for a book', async () => {
+      fetchMock
+        .mockResponseOnce(JSON.stringify([harryPotter, greatGatsby]))
+        .mockResponseOnce(JSON.stringify([greatGatsby]));
+
+      const { getByText, getByLabelText } = render(<App />);
+
+      expect(fetchMock.mock.calls.length).toEqual(1);
+
+      // after API call is resolved, it should display the book
+      await waitFor(() => getByText('Harry Potter'));
+
+      const searchInput = getByLabelText('Search for a book');
+
+      fireEvent.change(searchInput, {
+        target: {
+          value: 'The Great Gatsby',
+        },
+      });
+
+      act(() => {
+        jest.advanceTimersByTime(500);
+      });
+
+      await waitFor(() => getByText('The Great Gatsby'));
+
+      expect(fetchMock.mock.calls[1][0]).toEqual(
+        'http://localhost:3001/books?q=The Great Gatsby'
+      );
+      expect(fetchMock.mock.calls.length).toEqual(2);
+    });
+  });
+
+  describe('deleting', () => {
+    it('should be able to delete', async () => {
+      fetchMock
+        .mockResponseOnce(JSON.stringify([{ id: '1', name: 'Harry Potter' }]))
+        .mockResponseOnce(JSON.stringify({}));
+      const { getByText } = render(<App />);
+
+      await waitFor(() => getByText('Harry Potter'));
+
+      getByText('Delete').click();
+      getByText('Confirm?').click();
+
+      await waitForElementToBeRemoved(() => getByText('Harry Potter'));
+    });
+  });
 });
--- react-testing/client/src/setupTests.ts
+++ react-testing/client/src/setupTests.ts
@@ -3,4 +3,5 @@
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom';
-global.fetch = require('jest-fetch-mock');
+import { enableFetchMocks } from 'jest-fetch-mock';
+enableFetchMocks();
